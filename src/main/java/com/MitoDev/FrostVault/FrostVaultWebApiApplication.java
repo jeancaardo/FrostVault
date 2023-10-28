3a466bbdaaca4c6c4236e229e17dce7695e9beae
jean <ext_jecardo@mercadolibre.com>
2023-10-28 18:16:24 -0300

    [feat] initial scaffolding
    [added] pom.xml with springboot, security, jpa, sql driver and web.

---
+++ src/main/java/com/MitoDev/FrostVault/FrostVaultWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.MitoDev.FrostVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostVaultWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostVaultWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MitoDev/FrostVault/FrostVaultWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MitoDev.FrostVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostVaultWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
